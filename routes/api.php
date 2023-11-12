9e935b3d7e33048ca3391ed53026918f4236c138
TrungDG <trungdg@ethan-tech.com>
2023-11-12 22:37:51 +0700

    Create function redirect to controller in route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+if (! function_exists('ctl')) {
+    function ctl(string $controller, string $method) {
+        $className = "App\\Http\\Controllers\\{$controller}Controller";
+        return [$className, $method];
+    }
+}
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::prefix('v1')->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('Get list user');
+    Route::get('/users', ctl('User', 'index'))->name('Get list user');
     Route::get('/user/{id}', [UserController::class, 'show'])->name('Get user detail');
     Route::post('/user', [UserController::class, 'store'])->name('Create new user');
     Route::put('/user/{id}', [UserController::class, 'update'])->name('Update user');
